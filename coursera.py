d861c7ff125afef62b6001ff45375d68a16ece02
datamatt <mattfornito@gmail.com>
2016-06-30 11:47:47 -0600

    Update coursera.py

--- coursera.py
+++ coursera.py
@@ -3,4 +3,4 @@
 
 pip install coursera-dl
 #insert your coursera username and password in respective brackets
-coursera-dl -u <username> -p <password> sna-2012-001 pgm-003 algs4partI-010 gametheory2-002 aofa-006 maththink-006 algs4partII-007 aiplan-003 automata compilers cs101 intrologic linearopt-002 machlearning-001 mmds-003 neuralnets-2012-001 nlp gametheory-005
+coursera-dl -u <email> -p <password> sna-2012-001 pgm-003 algs4partI-010 gametheory2-002 aofa-006 maththink-006 algs4partII-007 aiplan-003 automata compilers cs101 intrologic linearopt-002 machlearning-001 mmds-003 neuralnets-2012-001 nlp gametheory-005
